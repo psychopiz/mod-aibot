b972ba7d58b812b80e4c7f9ad8ef8be7b3606a5a
PsychoPiz <4604198@qq.com>
2022-11-24 15:35:14 +0800

    Update aibot_loader.cpp

--- src/aibot_loader.cpp
+++ src/aibot_loader.cpp
@@ -1,1 +1,16 @@
+void AddAuctionHouseBotCommandScripts();
+void AddAuctionHouseBotScripts();
+void AddAccountBotCommandScripts();
+void AddAccountBotScripts();
+void AddRandomBotCommandScripts();
+void AddRandomBotScripts();
 
+void Addmod_aibotScripts()
+{
+    AddAuctionHouseBotCommandScripts();
+    AddAuctionHouseBotScripts();
+    AddAccountBotCommandScripts();
+    AddAccountBotScripts();
+    AddRandomBotCommandScripts();
+    AddRandomBotScripts();
+}
